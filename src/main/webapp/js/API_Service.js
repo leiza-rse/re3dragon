17686e8234fc0c71a32e482bdeb684a3b0844b9e
Florian Thiery <web@florian-thiery.de>
2021-01-28 14:25:37 +0100

    add dragonitems.html

--- src/main/webapp/js/API_Service.js
+++ src/main/webapp/js/API_Service.js
@@ -28,3 +28,57 @@ API.getDragonItemData = (callback, uri, spinner, target) => {
         });
     }, 0);
 };
+
+API.getDragonItemDataSearch = (callback, q, repo, spinner, target) => {
+    setTimeout(function() {
+        $.ajax({
+            type: 'GET',
+            async: false,
+            url: API.BASE + "/search?q=" + q + "&repo=" + repo,
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error(errorThrown);
+            },
+            success: function(response) {
+                try {
+                    response = JSON.parse(response);
+                } catch (e) {}
+                if (typeof callback === 'function') {
+                    if (spinner != null && target != null) {
+                        callback(spinner, target, response);
+                    } else {
+                        callback(response, q, repo);
+                    }
+                } else {
+                    return response;
+                }
+            }
+        });
+    }, 0);
+};
+
+API.getDragonItemDataLookUp = (callback, ids, spinner, target) => {
+    setTimeout(function() {
+        $.ajax({
+            type: 'GET',
+            async: false,
+            url: API.BASE + "/items?ids=" + ids,
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error(errorThrown);
+            },
+            success: function(response) {
+                try {
+                    response = JSON.parse(response);
+                } catch (e) {}
+                if (typeof callback === 'function') {
+                    if (spinner != null && target != null) {
+                        callback(spinner, target, response);
+                    } else {
+                        callback(response, "", "");
+                    }
+                } else {
+                    return response;
+                }
+            }
+        });
+    }, 0);
+};

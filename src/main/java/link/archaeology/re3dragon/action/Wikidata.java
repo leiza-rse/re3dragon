6b6be2729e6f4bb226365884b7e20cfb3f1b520a
Florian Thiery <web@florian-thiery.de>
2021-02-16 12:11:10 +0100

    Update Wikidata.java

--- src/main/java/link/archaeology/re3dragon/action/Wikidata.java
+++ src/main/java/link/archaeology/re3dragon/action/Wikidata.java
@@ -154,14 +154,14 @@ public class Wikidata {
             return error;
         }
     }
-    
+
     public static JSONArray items(String ids) throws IOException, ResourceNotAvailableException, ParseException, RetcatException {
         try {
             // set ids
             String[] id_array = ids.split(",");
             String id_list = "";
             for (String element : id_array) {
-                id_list += "<" + element.replace("(", "%2528").replace(")", "%2529") + ">,";
+                id_list += element.replace("http://www.wikidata.org/entity/", "wd:") + " ";
             }
             id_list = id_list.substring(0, id_list.length() - 1);
             // output
@@ -176,20 +176,11 @@ public class Wikidata {
             sparql += "PREFIX wikibase: <http://wikiba.se/ontology#> ";
             sparql += "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ";
             sparql += "PREFIX schema: <http://schema.org/> ";
-            sparql += "SELECT ?s ?displaylabel ?displaydesc ?wkt ?lat ?lon { ";
-            sparql += "?s rdfs:label ?displaylabel. ";
-            sparql += "?s schema:description ?displaydesc. ";
-            //sparql += "?s rdfs:label ?preflabel. ";
-            //sparql += "?s schema:description ?desc. ";
-            sparql += "FILTER (lang(?displaydesc) = 'en') ";
-            sparql += "FILTER (lang(?displaylabel) = 'en') ";
-            //sparql += "FILTER (lang(?preflabel) = lang(?desc)) ";
-            sparql += "FILTER (?s IN (" + id_list + ")) ";
-            //sparql += "OPTIONAL { ?s wdt:P31 ?broader. ?broader rdfs:label ?broaderlabel. FILTER(lang(?broaderlabel) = 'en')} ";
-            //sparql += "OPTIONAL { ?s wdt:P279 ?broader. ?broader rdfs:label ?broaderlabel. FILTER(lang(?broaderlabel) = 'en')} ";
-            //sparql += "OPTIONAL { ?narrower wdt:P31 ?s. ?narrower rdfs:label ?narrowerlabel. FILTER(lang(?narrowerlabel) = 'en')} ";
-            //sparql += "OPTIONAL { ?narrower wdt:P279 ?s. ?narrower rdfs:label ?narrowerlabel. FILTER(lang(?narrowerlabel) = 'en')} ";
-            //sparql += "OPTIONAL { ?s wdt:P625 ?wkt. ?s p:P625 ?geostatement . ?geostatement psv:P625 ?coordinate_node . ?coordinate_node wikibase:geoLatitude ?lat . ?coordinate_node wikibase:geoLongitude ?lon . } ";
+            sparql += "PREFIX bd: <http://www.bigdata.com/rdf#> ";
+            sparql += "SELECT ?item ?itemLabel ?itemDescription ?wkt ?lat ?lon { ";
+            sparql += "VALUES ?item { " + id_list + " } ";
+            sparql += "SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" } ";
+            sparql += "OPTIONAL { ?item wdt:P625 ?wkt. ?item p:P625 ?geostatement . ?geostatement psv:P625 ?coordinate_node . ?coordinate_node wikibase:geoLatitude ?lat . ?coordinate_node wikibase:geoLongitude ?lon . } ";
             sparql += " }";
             URL obj = new URL(sparqlendpoint);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -219,7 +210,7 @@ public class Wikidata {
             if (bindingsArray.size() > 0) {
                 for (Object element : bindingsArray) {
                     JSONObject tmpElement = (JSONObject) element;
-                    JSONObject s = (JSONObject) tmpElement.get("s");
+                    JSONObject s = (JSONObject) tmpElement.get("item");
                     String sValue = (String) s.get("value");
                     elements.add(sValue);
                 }
@@ -232,10 +223,10 @@ public class Wikidata {
             if (bindingsArray.size() > 0) {
                 for (Object element : bindingsArray) {
                     JSONObject tmpElement = (JSONObject) element;
-                    JSONObject s = (JSONObject) tmpElement.get("s");
+                    JSONObject s = (JSONObject) tmpElement.get("item");
                     String sValue = (String) s.get("value");
                     // set preflabel
-                    JSONObject label = (JSONObject) tmpElement.get("displaylabel");
+                    JSONObject label = (JSONObject) tmpElement.get("itemLabel");
                     String labelValue = (String) label.get("value");
                     String labelLang = (String) label.get("xml:lang");
                     for (Map.Entry me : dragonItems.entrySet()) {
@@ -245,7 +236,7 @@ public class Wikidata {
                         }
                     }
                     // set prefdesc
-                    JSONObject scopenote = (JSONObject) tmpElement.get("displaydesc");
+                    JSONObject scopenote = (JSONObject) tmpElement.get("itemDescription");
                     if (scopenote != null) {
                         String scopenoteValue = (String) scopenote.get("value");
                         String scopenoteLang = (String) scopenote.get("xml:lang");
@@ -256,11 +247,25 @@ public class Wikidata {
                             }
                         }
                     }
-                    // set additional information from triplestore
-                    for (Map.Entry me : dragonItems.entrySet()) {
-                        if (sValue.equals(me.getKey())) {
-                            DragonItem tmp = (DragonItem) me.getValue();
-                            tmp.setLairInfo("7D2HP57S");
+                    // set location
+                    JSONObject lat = (JSONObject) tmpElement.get("lat");
+                    JSONObject lon = (JSONObject) tmpElement.get("lon");
+                    if (lat != null && lon != null) {
+                        String latValue = (String) lat.get("value");
+                        String lonValue = (String) lon.get("value");
+                        GeoJSONFeature gjf = new GeoJSONFeature("Point", Double.parseDouble(lonValue), Double.parseDouble(latValue), 4326);
+                        for (Map.Entry me : dragonItems.entrySet()) {
+                            if (sValue == me.getKey()) {
+                                DragonItem tmp = (DragonItem) me.getValue();
+                                tmp.setLocation(gjf.getGeoJSONFeaturePoint());
+                            }
+                        }
+                        // set additional information from triplestore
+                        for (Map.Entry me : dragonItems.entrySet()) {
+                            if (sValue.equals(me.getKey())) {
+                                DragonItem tmp = (DragonItem) me.getValue();
+                                tmp.setLairInfo("7D2HP57S");
+                            }
                         }
                     }
                 }
@@ -274,7 +279,7 @@ public class Wikidata {
             return dragonItems_out;
         } catch (Exception e) {
             JSONParser parser = new JSONParser();
-            JSONObject error = (JSONObject) parser.parse(Logging.getMessageJSON(e, "link.archaeology.re3dragon.action.GettyAAT"));
+            JSONObject error = (JSONObject) parser.parse(Logging.getMessageJSON(e, "link.archaeology.re3dragon.action.Wikidata"));
             JSONArray error_arr = new JSONArray();
             error_arr.add(error);
             return error_arr;
